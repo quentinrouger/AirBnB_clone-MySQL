21ff50ef5365708ebc6c946bc4636bdc09d71136
chaher13 <chahercrbs@gmail.com>
2023-08-03 08:38:32 +0200

    update: add the second inherit of User between bracket

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
@@ -11,4 +11,4 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True) 
+    last_name = Column(String(128), nullable=True)
