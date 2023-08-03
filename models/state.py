218bcff236e838e72dc8b52ad7e6ded6f35c9cf5
chaher13 <chahercrbs@gmail.com>
2023-08-03 11:06:29 +0200

    Add: for DBStorage: class attribute cities must represent a relationship with the class City. If the State object is deleted, all linked City objects must be automatically deleted. Also, the reference from a City object to his State should be named state,
    for FileStorage: getter attribute cities that returns the list of City instances with state_id equals to the current State.id => It will be the FileStorage relationship between State and City
    New engine DBStorage: (models/engine/db_storage.py)

--- models/state.py
+++ models/state.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+import models
+from os import getenv
 from models.base_model import BaseModel
+from models.city import City
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -12,3 +16,13 @@ class State(BaseModel):
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade='all, delete-orphan',
                           backref='state')
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
