af26f897c9c7512e479ab526b5be146e11355a38
chaher13 <chahercrbs@gmail.com>
2023-08-14 12:52:19 +0200

    Add:  a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This is a script thats starts a Flask web application"""
+
+
+from flask import Flask
+
+blog_app = Flask(__name__)
+
+@blog_app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "<p>Hello HBNB!</p>"
+
+if __name__ == "__main__":
+    blog_app.run(host="0.0.0.0", port=5000)
+
