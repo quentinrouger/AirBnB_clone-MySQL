b4452dfa4c52f450f954896eaae8c1d7ad6d05ea
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-17 13:10:08 +0200

    Add: Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""This is a script thats starts a Flask web applications"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+blog_app = Flask(__name__)
+
+
+@blog_app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template('7-states_list.html', states_list=storage.all(State))
+
+@blog_app.teardown_appcontext
+def tear_down(self):
+    storage.close()
+
+if __name__ == "__main__":
+    blog_app.run(host='0.0.0.0', port='5000')
