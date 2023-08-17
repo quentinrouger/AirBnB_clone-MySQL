5778377625b308e6079d4d31e23cb73b0d919003
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-17 10:41:20 +0200

    Add: Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""This is a script thats starts a Flask web application"""
+
+
+from flask import Flask
+
+blog_app = Flask(__name__)
+
+
+@blog_app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@blog_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@blog_app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace("_", " ")
+    return "C " + text
+
+
+@blog_app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    text = text.replace("_", " ")
+    return "Python " + text
+
+if __name__ == "__main__":
+    blog_app.run(host='0.0.0.0', port='5000')
